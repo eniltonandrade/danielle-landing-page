48c2f0c1151a23438b6c54504cdbf11a469dea64
Enilton Rodrigues de Andrade <erodri@br.ibm.com>
2024-09-27 16:25:33 -0300

    updates next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    transpilePackages: ['lucide-react'],
-}
+    transpilePackages: ['lucide-react'], // add this
+    output: 'export',
+    distDir: 'dist',
+    images: {
+      unoptimized: true,
+    }
+  }
 
 export default nextConfig;
