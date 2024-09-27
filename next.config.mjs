6d2aa9722362e652106b74993a16404afbb4e035
Enilton Rodrigues de Andrade <erodri@br.ibm.com>
2024-09-27 16:11:36 -0300

    removes github pages workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-    basePath:'/danielle-landing-page',
-    transpilePackages: ['lucide-react'], // add this
-    output: 'export',
-    distDir: 'dist',
-    images: {
-      unoptimized: true,
-    }
-  }
+    transpilePackages: ['lucide-react'],
+}
 
 export default nextConfig;
