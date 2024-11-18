828ffa3108d46c75ccb8c2b40c1236b0bc95cbc3
Enilton Rodrigues de Andrade <erodri@br.ibm.com>
2024-11-18 18:12:02 -0300

    adiciona gtag

--- src/components/gtag.tsx
+++ src/components/gtag.tsx
@@ -1,11 +1,14 @@
-
-import Script from 'next/script'
+import Script from "next/script";
 
 export const GTag = () => {
   return (
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=G-V6NLCS5BLC`}
+        strategy="afterInteractive"
+      />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=AW-16724522700`}
         strategy="afterInteractive"
       />
 
@@ -17,6 +20,15 @@ export const GTag = () => {
           gtag('config', 'G-V6NLCS5BLC');
         `}
       </Script>
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16724522700');
+        `}
+      </Script>
     </>
-  )
-}
+  );
+};
